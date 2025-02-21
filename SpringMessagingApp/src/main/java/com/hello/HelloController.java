88dfa58e836bc894feed3fb7fbb1ab4911aee2a3
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-21 23:24:50 +0530

    Making rest call using put request method and pass first name as path variable and last name as query parameter

--- SpringMessagingApp/src/main/java/com/hello/HelloController.java
+++ SpringMessagingApp/src/main/java/com/hello/HelloController.java
@@ -1,7 +1,6 @@
 package com.hello;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -9,5 +8,25 @@ public class HelloController {
     @RequestMapping("/uc1")
     public String sayHello(){
         return "Hello from BridgeLabz!";
+    }
+
+    @RequestMapping("/uc2")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @RequestMapping("/uc3/{name}")
+    public String sayHelloPath(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @PostMapping("/uc4")
+    public String sayHelloPost(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }
